510e11a40e6a0d73b9f08513d61571fb5311a71c
Sumit Yadav <careeratedjing@gmail.com>
2021-12-12 11:08:58 +0530

    basic support added

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
+from dotenv import load_dotenv
 
 from resources.home import Home
 from resources.farmer import UploadCSV
 
+load_dotenv()
 
 app = Flask(__name__)
 api = Api(app)
+
 api.add_resource(Home, '/')
 api.add_resource(UploadCSV, '/upload')
 
